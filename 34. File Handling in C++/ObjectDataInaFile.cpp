66db20a3f55590ceb5ae458a9ba4b69b7362ab59
humaid29ahmed <humaid29masood@gmail.com>
2025-02-17 16:53:53 +0530

    Added the Updating Book Functionality in ObjectDataInaFile.cpp

--- 34. File Handling in C++/ObjectDataInaFile.cpp
+++ 34. File Handling in C++/ObjectDataInaFile.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
 
 using namespace std;
@@ -37,6 +39,7 @@ class Book{
         void viewAllBooks();
         void searchBook(const char*);
         void deleteBook(const char*);
+        void updateBook(const char*);
 
 };
 
@@ -117,10 +120,9 @@ void Book::deleteBook(const char* t)
 
     while(fin.read((char*)this, sizeof(*this)))
     {
-        cout<<"Record Written"<<endl;
         if(strcmp(t,title))
         {
-            cout<<"Record Written"<<endl;
+            
             fout.write((char*)this, sizeof(*this));
         }
     }
@@ -129,22 +131,134 @@ void Book::deleteBook(const char* t)
     remove("Book.txt");
     rename("tempfile.txt", "Book.txt");
 }
+
+// void Book::updateBook(const char* t)
+// {
+//     cout<<"Recording.....";
+//     fstream file;
+//     int counter = 0;
+//     file.open("Book.txt", ios::in|ios::ate|ios::out|ios::binary);
+//     if (!file) {
+//         cerr << "File error!" << std::endl;
+//     }else{
+//         file.seekg(0);
+//         file.read((char*)this, sizeof(*this));
+//     while(!file.eof())
+//     {
+//         if(!strcmp(t,title))
+//         {
+//             getBookdata();
+//             counter++;
+//             // Move back by the size of the object to overwrite
+//             int currentpos = file.tellg();
+//             int objectSize = sizeof(*this);
+//             cout<<currentpos<<" "<<objectSize;
+//             file.seekg(currentpos - objectSize);
+//             file.write((char*)this,sizeof(*this));
+//         }
+//         file.read((char*)this, sizeof(*this));
+//     }
+//     if(counter == 0)
+//     {
+//         cout<<"\nRecord not Found!\n";
+//     }
+//     file.close();
+// }
+// }
+void Book::updateBook(const char* t)
+{
+    fstream file;
+    int counter = 0;
+    file.open("Book.txt", ios::in | ios::out | ios::binary);
+    if (!file) {
+        cerr << "File error!" << std::endl;
+        return;
+    }
+
+    // Read and search for the book
+    while(file.read((char*)this, sizeof(*this))) {
+        if(!strcmp(t, title)) {
+            cout << "Enter new book details:\n";
+            getBookdata();
+            counter++;
+
+            // Move the file pointer back to the start of the record
+            file.seekp(-sizeof(*this), ios::cur);
+            file.write((char*)this, sizeof(*this));
+            break;
+        }
+    }
+
+    if(counter == 0) {
+        cout << "\nRecord not Found!\n";
+    }
+
+    file.close();
+}
+
+int menu()
+{
+    int choice;
+    cout<<"\n1. Add a Book into a Record\n";
+    cout<<"2. Show the Book Record Data\n";
+    cout<<"3. Search a Book in the Book Record\n";
+    cout<<"4. Delete a Book in the Book Record\n";
+    cout<<"5. Update a Book Data in the Book Record\n";
+    cout<<"6. Exit\n";
+    cout<<"Enter your Choice: ";
+    cin>>choice;
+    cout<<"\n";
+    return choice;
+}
 int main()
 {
-    Book b1,b2;
-
-    // b1.getBookdata();
-    // // // b1.showBookData();
-    // b1.storeBook();
-    // // // b2.showBookData();
-    // // b2.storeBook();
-    // b1.viewAllBooks();
-    // b1.searchBook("1001 Nights");
-    // cout<<"\n-------------------------------------------------------------------------------\n";
+  
+    Book book;
+    char title[20];
+   while(1)
+   {
+    switch(menu()){
+        case 1:
+           book.getBookdata();
+           book.storeBook();
+           cout<<"\nSucessfully Added the Record\n";
+        break;
+        case 2:
+           book.viewAllBooks();
+        break;
+        case 3:
+            cout<<"\nEnter the title of the book : ";
+            cin.ignore();
+            cin.getline(title,19);
+            book.searchBook(title);
+        break;
+        case 4:
+            cout<<"\nEnter the title of the book : ";
+            cin.ignore();
+            cin.getline(title,19);
+            book.deleteBook(title);
+            cout<<"\nRecord has been Sucessfully deleted\n";
+        break;
+        case 5:
+            cout<<"\nEnter the title of the book : ";
+            cin.ignore();
+            cin.getline(title,19);
+            book.updateBook(title);
+            cout<<"\nRecord has been Sucessfully Updated\n";
+        break;
+        case 6:
+            cout<<"\nThanks for Using this Application";
+            cout<<"\nPress Any Key to Exit\n ";
+            getch();
+            exit(0);
+        break;
+        default:
+        cout<<"\nIvalid Choice\n";
+        break;
+    }
     
-    b1.deleteBook("1001 Nights");
-    cout<<"\n-------------------------------------------------------------------------------\n";
-    b1.viewAllBooks();
+   }
+
 
     return 0;
 }
